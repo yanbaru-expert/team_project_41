7d3ade8a578f671c49c7185cbbddfe97f21066a5
nakamura <ynakamara04@gmail.com>
2021-01-29 22:36:12 +0900

    delete

--- lib/autoloads/import_csv.rb
+++ lib/autoloads/import_csv.rb
@@ -2,7 +2,7 @@ class ImportCsv
   # CSVデータのパスを引数として受け取り、インポート処理を実行
   def self.import(path)
      CSV.foreach(path, headers: true) do |row|
-       User.create!(
+       Post.create!(
          genre: row["genre"],
          title: row["title"],
          url: row["url"]
