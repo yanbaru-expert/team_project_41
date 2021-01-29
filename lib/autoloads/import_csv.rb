0139c4b16c17596aca27c467813f0db89853a509
nakamura <ynakamara04@gmail.com>
2021-01-29 13:25:55 +0900

    データ投入用ファイルにコードを追加

---
+++ lib/autoloads/import_csv.rb
@@ -0,0 +1,12 @@
+class ImportCsv
+  # CSVデータのパスを引数として受け取り、インポート処理を実行
+  def self.import(path)
+     CSV.foreach(path, headers: true) do |row|
+       User.create!(
+         genre: row["genre"],
+         title: row["title"],
+         url: row["url"]
+       )
+     end
+  end
+end
