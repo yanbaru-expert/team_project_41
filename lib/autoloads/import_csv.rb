c4f28a8e37cecc1aef4fced39cd5f9f2e21a44a3
nakamura <ynakamara04@gmail.com>
2021-01-29 23:36:18 +0900

    動画教材のCSVインポート機能を実装

--- lib/autoloads/import_csv.rb
+++ lib/autoloads/import_csv.rb
@@ -1,12 +1,18 @@
 class ImportCsv
   # CSVデータのパスを引数として受け取り、インポート処理を実行
   def self.import(path)
-     CSV.foreach(path, headers: true) do |row|
-       Post.create!(
-         genre: row["genre"],
-         title: row["title"],
-         url: row["url"]
-       )
-     end
+    list = []
+    CSV.foreach(path, headers: true) do |row|
+      list << row.to_h
+    end
+    list
+  end
+
+  def self.movie_data
+    list =  import('db/csv_data/movie_data.csv')
+
+    puts "インポート処理を開始"
+    Movie.create!(list)
+    puts "インポート完了"
   end
 end
