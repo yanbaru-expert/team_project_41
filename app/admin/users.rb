3c19147f6ac160cc0976e2f4432451a60681f5dd
Izushi <shoeshinesamurai@gmail.com>
2021-02-03 17:37:53 +0900

    管理者画面の実装

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register User do
-  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
+  permit_params :email, :password, :password_confirmation
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 end
