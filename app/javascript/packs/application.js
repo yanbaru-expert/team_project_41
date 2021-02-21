25e7a3bd643d81be7c76f041dd6918ca9b683ca0
hayato-1221 <hiperformance888@gmail.com>
2021-02-21 23:19:42 +0900

    検索機能の実装と見た目を整える

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,18 @@ import "bootstrap/dist/js/bootstrap"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('turbolinks:load', () => {
+    const search = document.getElementById('text-search')
+    const tableRows = document.querySelectorAll('#card-body')
+
+    const filterTableRows = (e) => {
+        const value = e.target.value.toLowerCase()
+        for(const tr of tableRows) {
+            const isVisible = tr.textContent.toLowerCase().includes(value)
+            tr.style.display = isVisible? "" : "none"
+        }
+    }
+
+    search.addEventListener('input', filterTableRows)
+})
